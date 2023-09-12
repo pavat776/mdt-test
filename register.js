07785265c2f76a8150dfa9144ae50bfda6091bfc
pavat776 <47842722+pavat776@users.noreply.github.com>
2023-09-12 10:31:20 +0700

    Add files via upload

---
+++ register.js
@@ -0,0 +1,10 @@
+window.onload = pageLoad;
+
+function pageLoad(){
+	var from = document.getElementById("clickme");
+    clickButton.onclick = myFunction;
+}
+
+function validateForm() {
+    //ถ้าตรวจสอบแล้วว่ามีการ register ไม่ถูกต้องให้ return false ด้วย
+}
